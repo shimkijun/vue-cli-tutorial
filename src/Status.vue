4b8bfa7d55cd6ce0ed08960c5e216879ee260de8
shimkijun <shimkijun@gmail.com>
2020-05-10 13:36:28 +0900

    vue beginner learning

---
+++ src/Status.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>전역으로 뿌리는 Component</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>안녕하세요</h1>
+    <Status />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+
+    }
+  }
+
+}
+</script>
